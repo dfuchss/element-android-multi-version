99da353ff51b63e4e9c0fd6c5ee46cfb3a52e452
Dominik Fuchß <develop@fuchss.org>
2023-02-05 17:57:14 +0100

    Create build Pipeline

---
+++ scripts/element.io.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+mkdir -p releases
+
+DEPLOY="$GITHUB_WORKSPACE/fdroid"
+PWD=$(pwd)
+LATEST=$(curl --silent "https://api.github.com/repos/vector-im/element-android/releases/latest" | jq -r .tag_name)
+echo "Latest Version $LATEST"
+
+if compgen -G "releases/*$LATEST*.apk" > /dev/null; then
+        echo "Version already present."
+        exit 0
+fi
+
+if [[ ! "$LATEST" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "New RC $LATEST .. skipping as no release .."
+        exit 0
+fi
+
+
+echo "Building Latest Version $LATEST"
+
+if [ ! -d "$DIR/element-android" ]; then
+        echo "Cloning repository .."
+        git clone -q https://github.com/vector-im/element-android
+fi
+
+echo "Switching to $LATEST"
+cd element-android && git clean -f -x -q && git reset -q --hard && git fetch -q && git checkout -q $LATEST && git clean -q -d -x -f
+
+cd $PWD/element-android
+echo "Applying Modifications in $DIR/element-android"
+sed -i 's/resValue \"string\", \"app_name\", \"Element\"/resValue "string", "app_name", "Matrix (KIT)"/g' vector-app/build.gradle
+sed -i 's/\/\/ signingConfig signingConfigs.release/signingConfig signingConfigs.release/g' vector-app/build.gradle
+sed -i 's/fdroid {/fdroid {\n            applicationIdSuffix \".fkit\"/g' vector-app/build.gradle
+
+# Fix package_name for google services
+sed -i 's/gplay {/gplay {\n            applicationIdSuffix \".gkit\"/g' vector-app/build.gradle
+sed -i 's/\"package_name\": \"im.vector.app\"/"package_name": "im.vector.app.gkit"/g' vector-app/src/gplay/release/google-services.json
+
+# Setup Signing
+cp ../apps.jks .
+sed -i '/signing.element.storePath.*/d' gradle.properties
+sed -i '/signing.element.storePassword.*/d' gradle.properties
+sed -i '/signing.element.keyId.*/d' gradle.properties
+sed -i '/signing.element.keyPassword.*/d' gradle.properties
+cat ../keystore.properties >> gradle.properties
+
+cd $PWD
+
+echo "RUN Build"
+docker run -t --rm -v $DIR/element-android:/app androidsdk/android-31 bash -c "cd /app/ && /app/gradlew -q assembleFdroidRelease assembleGplayRelease"
+
+echo "Saving Release $LATEST"
+cp element-android/vector-app/build/outputs/apk/fdroid/release/vector-fdroid-universal-release.apk "releases/matrix-fdroid-$LATEST.apk"
+cp element-android/vector-app/build/outputs/apk/gplay/release/vector-gplay-universal-release.apk "releases/matrix-gplay-$LATEST.apk"
+
+echo "Saving Release for Deployment to $DEPLOY"
+mkdir -p $DEPLOY/repo
+install releases/matrix-fdroid-$LATEST.apk $DEPLOY/repo/
+install releases/matrix-gplay-$LATEST.apk $DEPLOY/repo/
+
+rm releases/*$LATEST*.apk
+VER=$(date +%s)
+echo $VER > releases/element-$LATEST.apk
+
+cd $DEPLOY
+META=$DEPLOY/metadata/im.vector.app.fkit.yml
+if [ -f "$META" ]; then
+        echo "Updating Meta Data"
+        sed -i "s/CurrentVersionCode.*/CurrentVersionCode: $VER/g" $META
+else
+        echo "Meta Data not present. Please update on your own"
+fi
+
+META=$DEPLOY/metadata/im.vector.app.gkit.yml
+if [ -f "$META" ]; then
+        echo "Updating Meta Data"
+        sed -i "s/CurrentVersionCode.*/CurrentVersionCode: $VER/g" $META
+else
+        echo "Meta Data not present. Please update on your own"
+fi
